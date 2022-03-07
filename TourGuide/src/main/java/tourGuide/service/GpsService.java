c772af31fe6e693a81aa68f1b3a33afdbc3cc533
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-03-07 14:10:19 +0100

    Add GpsService interface

---
+++ TourGuide/src/main/java/tourGuide/service/GpsService.java
@@ -0,0 +1,18 @@
+package tourGuide.service;
+
+import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service Interface to retrieve users and attractions location and manage distance calculation.
+ */
+@Service
+public interface GpsService {
+
+  Map<Attraction, Double> getAttractionsWithDistances(Location location);
+
+  Map<Attraction, Double> getTopNearbyAttractionsWithDistances(Location location, int top);
+
+}
