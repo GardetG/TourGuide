9f4bd8c8ab138219e2a67c70a4f2a90796079e0a
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-03-10 15:24:19 +0100

    Implement TourGuideController getAllCurrentLocations() unit test

--- TourGuide/src/main/java/tourGuide/controller/TourGuideController.java
+++ TourGuide/src/main/java/tourGuide/controller/TourGuideController.java
@@ -3,6 +3,8 @@ package tourGuide.controller;
 import com.jsoniter.output.JsonStream;
 import gpsUtil.location.VisitedLocation;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tourGuide.domain.User;
+import tourGuide.dto.LocationDto;
 import tourGuide.dto.NearbyAttractionsDto;
 import tourGuide.dto.ProviderDto;
 import tourGuide.dto.UserPreferencesDto;
@@ -46,18 +49,8 @@ public class TourGuideController {
   }
 
   @RequestMapping("/getAllCurrentLocations")
-  public String getAllCurrentLocations() {
-    // TODO: Get a list of every user's most recent location as JSON
-    //- Note: does not use gpsUtil to query for their current location,
-    //        but rather gathers the user's current location from their stored location history.
-    //
-    // Return object should be the just a JSON mapping of userId to Locations similar to:
-    //     {
-    //        "019b04a9-067a-4c76-8817-ee75088c3822": {"longitude":-48.188821,"latitude":74.84371}
-    //        ...
-    //     }
-
-    return JsonStream.serialize("");
+  public Map<UUID, LocationDto> getAllCurrentLocations() {
+    return tourGuideService.getAllCurrentLocations();
   }
 
   @RequestMapping("/getTripDeals")
