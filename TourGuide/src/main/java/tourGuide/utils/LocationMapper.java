52b87d91b2f12fd7b5e40945c73cdd0018037f38
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-03-10 15:43:38 +0100

    Add LocationMapper

---
+++ TourGuide/src/main/java/tourGuide/utils/LocationMapper.java
@@ -0,0 +1,41 @@
+package tourGuide.utils;
+
+import gpsUtil.location.Location;
+import tourGuide.dto.LocationDto;
+
+/**
+ * Mapper utility class to map Location DTO and entity.
+ */
+public class LocationMapper {
+
+  private LocationMapper() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  /**
+   * Map a Location entity into DTO.
+   *
+   * @param location to map
+   * @return corresponding LocationDto mapped
+   */
+  public static LocationDto toDto(Location location) {
+    return new LocationDto(
+        location.longitude,
+        location.latitude
+    );
+  }
+
+  /**
+   * Map a Location Dto into entity.
+   *
+   * @param locationDto to map
+   * @return corresponding Location mapped
+   */
+  public static Location toEntity(LocationDto locationDto) {
+    return new Location(
+        locationDto.getLatitude(),
+        locationDto.getLongitude()
+    );
+  }
+
+}
