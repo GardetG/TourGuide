7b9615af984fb4ee977d5ab72f4256fead2a5325
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-03-14 15:30:24 +0100

    Add NoLocationFoundException

---
+++ TourGuide/src/main/java/tourGuide/exception/NoLocationFoundException.java
@@ -0,0 +1,12 @@
+package tourGuide.exception;
+
+/**
+ * Exception thrown when no visited location found for the requested user.
+ */
+public class NoLocationFoundException extends Exception {
+
+  public NoLocationFoundException(String s) {
+    super(s);
+  }
+
+}
