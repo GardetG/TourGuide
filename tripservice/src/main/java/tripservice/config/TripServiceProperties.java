eee0a253dfc14dc7b487b7492af984e86f641237
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-03-28 16:20:18 +0200

    Add TripService properties

---
+++ tripservice/src/main/java/tripservice/config/TripServiceProperties.java
@@ -0,0 +1,24 @@
+package tripservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *  Configuration Class for TripService properties:
+ *  - tripPricerApiKey : Api Key used required by TripPricer library
+ */
+@Configuration
+@ConfigurationProperties(prefix = "tripservice")
+public class TripServiceProperties {
+
+  private String tripPricerApiKey;
+
+  public String getTripPricerApiKey() {
+    return tripPricerApiKey;
+  }
+
+  public void setTripPricerApiKey(String tripPricerApiKey) {
+    this.tripPricerApiKey = tripPricerApiKey;
+  }
+
+}
