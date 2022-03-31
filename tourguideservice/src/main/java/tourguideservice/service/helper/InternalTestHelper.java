b94ec808336c23e378fbde0d0cf98b50b0449215
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-03-31 18:36:22 +0200

    Update InternalTestHelper to user LocationServiceProxy

--- tourguideservice/src/main/java/tourguideservice/service/helper/InternalTestHelper.java
+++ tourguideservice/src/main/java/tourguideservice/service/helper/InternalTestHelper.java
@@ -1,68 +1,75 @@
-package tourguideservice.helper;
+package tourguideservice.service.helper;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import tourguideservice.domain.User;
 import shared.dto.LocationDto;
 import shared.dto.VisitedLocationDto;
 import tourguideservice.repository.UserRepository;
-import tourguideservice.service.GpsService;
+import tourguideservice.service.proxy.LocationServiceProxy;
 
 /**
  * Configuration Class to handle generation of the internal user map when using internal users for
  * testing purposes.
  */
-@Profile("internalUser")
-@Configuration
-@ConfigurationProperties(prefix = "tourguide.internaluser")
+@Profile("test")
+@Component
 public class InternalTestHelper {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(InternalTestHelper.class);
 	private final UserRepository userRepository;
-	private final GpsService gpsService;
+	private final LocationServiceProxy locationServiceProxy;
 	private final Random random = new Random();
 
 	@Autowired
 	public InternalTestHelper(UserRepository userRepository,
-							  GpsService gpsService) {
+							  LocationServiceProxy locationServiceProxy) {
 		this.userRepository = userRepository;
-		this.gpsService = gpsService;
-	}
-
-	public void setInternalUserNumber(int internalUserNumber) {
-		initializeInternalUsers(internalUserNumber);
+		this.locationServiceProxy = locationServiceProxy;
 	}
 
 	public void initializeInternalUsers(int internalUserNumber) {
 		userRepository.deleteAll();
-		IntStream.range(0, internalUserNumber).forEach(i -> {
-			String userName = "internalUser" + i;
-			String phone = "000";
-			String email = userName + "@tourGuide.com";
-			User user = new User(UUID.randomUUID(), userName, phone, email);
-			generateUserLocationHistory(user);
-
-			userRepository.save(user);
-		});
+		Executor internalUserExecutor = Executors.newFixedThreadPool(200);
+		List<CompletableFuture<Void>> userFutur = IntStream.range(0, internalUserNumber)
+				.mapToObj(i -> CompletableFuture.runAsync(() -> generateUser(i), internalUserExecutor))
+				.collect(Collectors.toList());
+		userFutur.forEach(CompletableFuture::join);
 		LOGGER.debug("Created {} internal test users.", internalUserNumber);
 	}
 
+	private void generateUser(int index) {
+		String userName = "internalUser" + index;
+		String phone = "000";
+		String email = userName + "@tourGuide.com";
+		User user = new User(UUID.randomUUID(), userName, phone, email);
+		generateUserLocationHistory(user);
+		userRepository.save(user);
+	}
+
 	private void generateUserLocationHistory(User user) {
-		IntStream.range(0, 3).forEach(i-> gpsService.addLocation(new VisitedLocationDto(
-				user.getUserId(),
-				new LocationDto(generateRandomLongitude(), generateRandomLatitude()),
-				getRandomTime()
-		)));
+		IntStream.range(0, 3).forEach(i-> {
+			VisitedLocationDto location = new VisitedLocationDto(
+					user.getUserId(),
+					new LocationDto(generateRandomLatitude(), generateRandomLongitude()),
+					getRandomTime()
+			);
+			locationServiceProxy.addLocation(location);
+		});
 	}
 
 	private double generateRandomLongitude() {
