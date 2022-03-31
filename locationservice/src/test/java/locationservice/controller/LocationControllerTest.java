2887e8c01847d4c76d7b61924fc8d433dc476b1c
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-03-31 14:44:57 +0200

    Add LocationController POST addLocation unit tests

--- locationservice/src/test/java/locationservice/controller/LocationControllerTest.java
+++ locationservice/src/test/java/locationservice/controller/LocationControllerTest.java
@@ -1,24 +1,36 @@
 package locationservice.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import locationservice.service.GpsService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import shared.dto.LocationDto;
+import shared.dto.PreferencesDto;
+import shared.dto.ProviderDto;
 import shared.dto.VisitedLocationDto;
 import shared.exception.NoLocationFoundException;
 
@@ -30,6 +42,9 @@ class LocationControllerTest {
 
     @MockBean
     private GpsService gpsService;
+
+    @Captor
+  ArgumentCaptor<VisitedLocationDto> visitedLocationCaptor;
 
   @DisplayName("GET last location should return 200 with user last visited location")
   @Test
@@ -88,6 +103,47 @@ class LocationControllerTest {
         .andExpect(jsonPath("$.location.longitude", is(-45.0)))
         .andExpect(jsonPath("$.timeVisited").isNotEmpty());
     verify(gpsService, times(1)).trackUserLocation(userId);
+  }
+
+  @DisplayName("POST visited location should return 200")
+  @Test
+  void addLocationTest() throws Exception {
+    // GIVEN
+    ObjectMapper objectMapper = new ObjectMapper();
+    UUID userId = UUID.randomUUID();
+    Date date = new Date();
+    VisitedLocationDto visitedLocation = new VisitedLocationDto(userId, new LocationDto(45,-45), date);
+
+    // WHEN
+    mockMvc.perform(post("/addLocation")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(visitedLocation)))
+
+        // THEN
+        .andExpect(status().isOk());
+    verify(gpsService, times(1)).addLocation(visitedLocationCaptor.capture());
+    assertThat(visitedLocationCaptor.getValue()).usingRecursiveComparison().isEqualTo(visitedLocation);
+  }
+
+  @DisplayName("POST invalid visited location should return 422")
+  @Test
+  void addLocationWhenInvalidTest() throws Exception {
+    // GIVEN
+    ObjectMapper objectMapper = new ObjectMapper();
+    VisitedLocationDto visitedLocation = new VisitedLocationDto(null, new LocationDto(100,-200), null);
+
+    // WHEN
+    mockMvc.perform(post("/addLocation")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(visitedLocation)))
+
+        // THEN
+        .andExpect(status().isUnprocessableEntity())
+        .andExpect(jsonPath("$.userId", is("User id is mandatory")))
+        .andExpect(jsonPath("$.['location.latitude']", is("Latitude can't be more than 90")))
+        .andExpect(jsonPath("$.['location.longitude']'", is("Longitude can't be less than -180")))
+        .andExpect(jsonPath("$.timeVisited", is("Time visited is mandatory")));
+    verify(gpsService, times(0)).addLocation(any(VisitedLocationDto.class));
   }
 
 }
