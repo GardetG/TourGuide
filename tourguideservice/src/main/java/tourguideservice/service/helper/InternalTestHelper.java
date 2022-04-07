d0f5bd63fc2fcad6c3e09c58d35ca746c981d62a
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-04-07 18:39:41 +0200

    Update InternalTestHelper to use UserServiceProxy

--- tourguideservice/src/main/java/tourguideservice/service/helper/InternalTestHelper.java
+++ tourguideservice/src/main/java/tourguideservice/service/helper/InternalTestHelper.java
@@ -16,11 +16,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import tourguideservice.domain.User;
+import shared.dto.UserDto;
+import shared.exception.UserNameAlreadyUsedException;
 import shared.dto.LocationDto;
 import shared.dto.VisitedLocationDto;
-import tourguideservice.repository.UserRepository;
 import tourguideservice.service.proxy.LocationServiceProxy;
+import tourguideservice.service.proxy.UserServiceProxy;
 
 /**
  * Configuration Class to handle generation of the internal user map when using internal users for
@@ -31,45 +32,53 @@ import tourguideservice.service.proxy.LocationServiceProxy;
 public class InternalTestHelper {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(InternalTestHelper.class);
-  private final UserRepository userRepository;
+  private final UserServiceProxy userServiceProxy;
   private final LocationServiceProxy locationServiceProxy;
   private final Random random = new Random();
 
   @Autowired
-  public InternalTestHelper(UserRepository userRepository,
+  public InternalTestHelper(UserServiceProxy userServiceProxy,
                             LocationServiceProxy locationServiceProxy) {
-    this.userRepository = userRepository;
+    this.userServiceProxy = userServiceProxy;
     this.locationServiceProxy = locationServiceProxy;
   }
 
   public void initializeInternalUsers(int internalUserNumber) {
-    userRepository.deleteAll();
     Executor internalUserExecutor = Executors.newFixedThreadPool(200);
     List<CompletableFuture<Void>> userFutur = IntStream.range(0, internalUserNumber)
-        .mapToObj(i -> CompletableFuture.runAsync(() -> generateUser(i), internalUserExecutor))
+        .mapToObj(i -> CompletableFuture.runAsync(() -> setUpUser(i), internalUserExecutor))
         .collect(Collectors.toList());
     userFutur.forEach(CompletableFuture::join);
     LOGGER.debug("Created {} internal test users.", internalUserNumber);
   }
 
-  private void generateUser(int index) {
+  private void setUpUser(int index) {
+    UUID userid = generateUser(index);
+    generateUserLocationHistory(userid);
+  }
+
+  private UUID generateUser(int index) {
     String userName = "internalUser" + index;
     String phone = "000";
     String email = userName + "@tourGuide.com";
-    User user = new User(UUID.randomUUID(), userName, phone, email);
-    generateUserLocationHistory(user);
-    userRepository.save(user);
+    UserDto user = new UserDto(null, userName, phone, email);
+    try {
+      return userServiceProxy.addUser(user).getUserId();
+    } catch (UserNameAlreadyUsedException e) {
+      LOGGER.error("Internal test users creation error : Duplicate name");
+      throw new IllegalStateException("Internal test users creation error");
+    }
   }
 
-  private void generateUserLocationHistory(User user) {
+  private void generateUserLocationHistory(UUID userId) {
     List<VisitedLocationDto> visitedLocationDtos = IntStream.range(0, 3)
         .mapToObj(i -> new VisitedLocationDto(
-            user.getUserId(),
+            userId,
             new LocationDto(generateRandomLatitude(), generateRandomLongitude()),
             getRandomTime()
         ))
         .collect(Collectors.toList());
-    locationServiceProxy.addVisitedLocation(visitedLocationDtos, user.getUserId());
+    locationServiceProxy.addVisitedLocation(visitedLocationDtos, userId);
   }
 
   private double generateRandomLongitude() {
