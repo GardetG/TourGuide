e382281ca9cc62914cda100ac7595a779b9af4b4
Gardet Gabriel <gabriel.ng@wanadoo.fr>
2022-04-07 14:34:25 +0200

    Add PreferencesService interface

---
+++ userservice/src/main/java/userservice/service/PreferencesService.java
@@ -0,0 +1,31 @@
+package userservice.service;
+
+import org.springframework.stereotype.Service;
+import shared.dto.PreferencesDto;
+import shared.exception.UserNotFoundException;
+
+/**
+ * Service Interface to manage users' preferences.
+ */
+@Service
+public interface PreferencesService {
+
+  /**
+   * Get the preferences of a user or throw an exception if the user can't be found.
+   *
+   * @param userName of the user
+   * @return Preferences Dto
+   * @throws UserNotFoundException when user not found
+   */
+  PreferencesDto getUserPreferences(String userName) throws UserNotFoundException;
+
+  /**
+   * Set the preferences of a user according to the provided Dto or throw an exception if the user
+   * can't be found.
+   *
+   * @param userName of the user
+   * @throws UserNotFoundException when user not found
+   */
+  void setUserPreferences(String userName, PreferencesDto preferencesDto) throws UserNotFoundException;
+
+}
